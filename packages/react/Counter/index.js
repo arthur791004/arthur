8fd94cc542e991f2ce9c3dec09278fab7ab8a221
arthur791004 <arthur791004@gmail.com>
2018-12-17 02:16:22 +0800

    init project

---
+++ packages/react/Counter/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Counter extends React.PureComponent {
+  state = {
+    count: 0,
+  };
+
+  increase = () => {
+    const { count } = this.state;
+
+    this.setState({
+      count: count + 1,
+    });
+  };
+
+  decrease = () => {
+    const { count } = this.state;
+
+    this.setState({
+      count: count - 1,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <span>{count}</span>
+        <div>
+          <button onClick={this.increase}>+</button>
+          <button onClick={this.decrease}>-</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter;
---
+++ packages/react/Counter/stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Counter from './';
+
+storiesOf('Counter', module)
+  .add('basic', () => <Counter />);
