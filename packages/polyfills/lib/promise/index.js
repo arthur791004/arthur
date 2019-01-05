c1213d227ba58b902fcbcc520f34dfc25fb80e27
arthur791004 <arthur791004@gmail.com>
2019-01-05 16:48:20 +0800

    implement then

--- packages/polyfills/lib/promise/index.js
+++ packages/polyfills/lib/promise/index.js
@@ -23,14 +23,75 @@ export default class Promise {
       rejectFn(reason);
     }
   }
+
+  then(onFulfillment, onRejection) {
+    // always return new Promise
+    return new this.constructor((resolve, reject) => {
+      this.onFulfillment = onFulfillment;
+      this.onRejection = onRejection;
+
+      if (this.state === STATE.PENDING) {
+        return;
+      }
+
+      try {
+        resolve.call(this, done(this));
+      } catch (reason) {
+        reject.call(this, reason);
+      }
+    });
+  }
 };
 
 function resolve(value) {
+  try {
+    const result = isThenable(value)
+      ? value.then()
+      : value;
+
+    fulfill.call(this, result);
+  } catch (reason) {
+    reject.call(this, reason);
+  }
+}
+
+function fulfill(value) {
   this.state = STATE.FULFILLED;
   this.result = value;
+
+  done(this);
 }
 
 function reject(reason) {
   this.state = STATE.REJECTED;
   this.result = reason;
+
+  done(this);
+}
+
+function done(promise) {
+  const { result } = promise;
+  const callback = getCallback(promise);
+
+  // TODO: callback should be function and executed asynchronously
+  if (typeof callback === 'function') {
+    callback(result);
+  }
+}
+
+function getCallback(promise) {
+  const { state, onFulfillment, onRejection } = promise;
+
+  switch (state) {
+    case STATE.FULFILLED:
+      return onFulfillment;
+    case STATE.REJECTED:
+      return onRejection;
+    default:
+      return;
+  }
+}
+
+function isThenable(value) {
+  return value && typeof value.then === 'function';
 }
