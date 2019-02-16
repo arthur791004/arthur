4631649def234571eb3f1b639eb0765d9f0af94c
arthur791004 <arthur791004@gmail.com>
2019-02-17 00:18:06 +0800

    implement scheduleUpdate

---
+++ packages/react/src/libs/createFiber.js
@@ -0,0 +1,12 @@
+export const FIBER_TAGS = {
+  FunctionComponent: 0,
+  ClassComponent: 1,
+  HostComponent: 2,
+  HostRoot: 3,
+};
+
+const createFiber = () => {
+
+};
+
+export default createFiber;
--- packages/react/src/libs/reconciler.js
+++ packages/react/src/libs/reconciler.js
@@ -1,3 +1,19 @@
-export const scheduleUpdate = () => {
+import { FIBER_TAGS } from './createFiber';
 
+const updateQueue = [];
+
+const performWork = (dealine) => {
+
+};
+
+export const scheduleUpdate = (instance, paritalState) => {
+  const update = {
+    tag: FIBER_TAGS.ClassComponent,
+    instance,
+    paritalState,
+  };
+
+  updateQueue.push(update);
+
+  requestIdleCallback(performWork);
 };
